3fb0dba25d3527e1fae74f45d6f5d8fc6a9ef51f
Dmitry <dpoliyivets@icloud.com>
2019-04-26 11:24:22 +0100

    Removed API key

--- app/constants/ApiKeys.js
+++ app/constants/ApiKeys.js
@@ -5,9 +5,9 @@ export default
     {
         FirebaseConfig:
         {
-            // apiKey: "AIzaSyBn2Qimf2zC6g9FP3GFQs8of43M_oivlN0",
-            // authDomain: "endoscopy-explorer-app.firebaseapp.com",
-            // databaseURL: "https://endoscopy-explorer-app.firebaseio.com",
-            // storageBucket: "endoscopy-explorer-app.appspot.com"
+            // apiKey: "",
+            // authDomain: "",
+            // databaseURL: "",
+            // storageBucket: ""
         }
     }
